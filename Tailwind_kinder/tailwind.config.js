971fab8bd2d772a30f55dc970cddfa253db661c6
Sunil Salaria <sunilsalaria1993@gmail.com>
2022-11-02 23:05:28 +0530

    school project tailwind

---
+++ Tailwind_kinder/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html","js/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Salsa: ["Salsa", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+}
